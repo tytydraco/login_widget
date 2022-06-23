04bbcc8c616e4b556c21038028e4c0594c05d122
Tyler Nijmeh <tylernij@gmail.com>
2022-06-23 12:31:36 -0700

    Refactor into package
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- android/app/src/main/kotlin/com/draco/login_widget/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.draco.login_widget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/README.md
@@ -0,0 +1,3 @@
+# example
+
+An example project using the [login_widget](https://www.github.com/tytydraco/flutter_login_widget) package
